14675336c5349c156663092e96ce9c5059370492
Jordan Dubrick <jdubrick@redhat.com>
2024-07-12 15:46:30 -0400

    change langchain
    Signed-off-by: Jordan Dubrick <jdubrick@redhat.com>

--- src/sample-app.py
+++ src/sample-app.py
@@ -4,7 +4,7 @@ import time
 
 import gradio as gr
 from langchain_openai import ChatOpenAI
-
+from langchain import OpenAI
 model_endpoint = os.getenv("MODEL_ENDPOINT", "http://localhost:8001")
 model_service = f"{model_endpoint}/v1"
 
@@ -26,7 +26,7 @@ def checking_model_service():
 checking_model_service()
 model_name = os.getenv("MODEL_NAME", "")
 
-llm = ChatOpenAI(base_url=model_service,
+llm = OpenAI(base_url=model_service,
                  model=model_name,
                  api_key="EMPTY",
                  streaming=True)
